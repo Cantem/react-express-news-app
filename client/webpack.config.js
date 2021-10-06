aa50517facc6a8596242f32a5874613592ddaec2
Rafal Porzucek <r.porzucek@gmail.com>
2021-10-06 20:57:52 +0100

    Configure webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,27 +1,58 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 const path = require('path');
-const babelConfig = require('./babel.config.json');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, './public/js')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|pubilc)/,
-        use: {
-          loader: 'babel-loader',
-          options: babelConfig
+module.exports = () => {
+  // call dotenv and it will return an Object with a parsed key
+  const env = dotenv.config().parsed;
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    mode: 'development',
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    },
+    devServer: { static: path.join(__dirname, 'src') },
+    module: {
+      rules: [
+        {
+          test: /\.?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|jp(e*)g|svg|gif)$/,
+          use: ['file-loader']
+        },
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack']
         }
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: ['style-loader', 'css-loader']
-      }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: path.join(__dirname, 'src', 'index.html')
+      }),
+      new webpack.DefinePlugin(envKeys)
     ]
-  }
+  };
 };
