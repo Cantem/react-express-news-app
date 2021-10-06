6b41e84662d8174ec3bee1ee4e218778b49921c1
Rafal Porzucek <r.porzucek@gmail.com>
2021-10-06 19:37:05 +0100

    format

--- server/src/services/ArticlesService.js
+++ server/src/services/ArticlesService.js
@@ -4,15 +4,19 @@ import { filterArticlesWithImages, modifyErrorLog } from '../utils/helpers';
 
 export class ArticlesService {
   async getArticles() {
-    const url = new URL(EXTERNAL_API.DOMAIN + EXTERNAL_API.GET_TOP_ARTICLES + `&apiKey=${process.env.NEWS_API_KEY}`);
+    const url = new URL(
+      EXTERNAL_API.DOMAIN +
+        EXTERNAL_API.GET_TOP_ARTICLES +
+        `&apiKey=${process.env.NEWS_API_KEY}`
+    );
     try {
       const response = await axios.get(url.href);
 
       return filterArticlesWithImages(response.data.articles);
-    } catch(error) {
+    } catch (error) {
       const err = modifyErrorLog(error);
       console.error('GET ARTICLES SERVICE', err);
-      throw error
+      throw error;
     }
   }
 }
