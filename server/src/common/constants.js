5346ec3b4052408c693e5363548820d5c7989fd6
Rafal Porzucek <r.porzucek@gmail.com>
2021-10-06 19:27:43 +0100

    Add cons for the external api

---
+++ server/src/common/constants.js
@@ -0,0 +1,6 @@
+export const EXTERNAL_API = Object.freeze({
+  DOMAIN: 'https://newsapi.org/v2/',
+  GET_TOP_ARTICLES: 'top-headlines?country=us&category=business',
+  SEARCH: 'everything?q=',
+  SORT_BY_PUBLISHED_AT: 'sortBy=publishedAt'
+});
