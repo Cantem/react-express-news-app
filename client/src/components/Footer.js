3b1321ef762b14a2da73a122503fb7709f232b6e
Rafal Porzucek <r.porzucek@gmail.com>
2021-10-07 15:11:18 +0100

    Align footer with layout

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,13 +3,19 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  container: {
+    width: '100%'
+  }
+});
+
 
 export default function Footer() {
-  const styles = {
-    width: '100vw'
-  };
+  const classes = useStyles();
   return (
-    <footer style={styles}>
+    <footer className={classes.container}>
       <Box
         px={{ xs: 3, sm: 10 }}
         py={{ xs: 5, sm: 10 }}
@@ -19,7 +25,7 @@ export default function Footer() {
         sx={{ fontSize: 20, fontWeight: 'medium' }}
       >
         <Container maxWidth="lg">
-          <Grid container spacing={5}>
+          <Grid container spacing={3}>
             <Grid item xs={12} sm={4}>
               <Box borderBottom={1} fontSize={16} fontWeight={700}>
                 Help
