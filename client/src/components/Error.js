345ffce4f1d0020be4d42c48de93de8e87ed243d
Rafal Porzucek <r.porzucek@gmail.com>
2021-10-11 17:04:43 +0100

    Remove underline from go home in Not Found

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Error = () => (
-  <div>
-    <h1>404 - Not Found!</h1>
-    <Link to="/">
-      <Button style={{ textDecoration: 'none'}} variant="contained" size="large" color="primary">
-        Go Home
-      </Button>
-    </Link>
-  </div>
-);
+const useStyles = makeStyles({
+  wrapper: {
+    height: '100%',
+    textAlign: 'center',
+  },
+  link: {
+    textDecoration: 'none' 
+  }
+});
+
+const Error = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <h1>404 - Not Found!</h1>
+      <Link to="/" className={classes.link}>
+        <Button variant="contained" size="large" color="primary">
+          Go Home
+        </Button>
+      </Link>
+    </div>
+  );
+};
 
 export default Error;
